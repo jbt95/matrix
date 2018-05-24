9c4efbba37b71e9a33732ceda10967213d5f346d
jbt95 <berme495@gmail.com>
2018-05-24 12:11:54 +0200

    lul

--- matrix.go
+++ matrix.go
@@ -2,9 +2,9 @@ package matrix
 
 import (
 	"fmt"
+	"log"
 )
 
-//row*ncols + col
 type Matrix struct {
 	data []float64
 	rows int
@@ -13,25 +13,41 @@ type Matrix struct {
 }
 
 //Returns a matrix from the given slice or an error if the number of cols and rows does not match the len of the slice
-func NewFromSlice(cols int, rows int, data []float64) (*Matrix, error) {
+func NewFromSlice(cols int, rows int, data []float64) Matrix {
 	if len(data) != rows*cols {
-		return nil, fmt.Errorf("The size of the matrix rows*cols: (%v) doesn't match the size of the given slice (%v)", rows*cols, len(data))
+		log.Fatalf("The size of the matrix rows*cols: (%v) doesn't match the size of the given slice (%v)", rows*cols, len(data))
 	}
-	m := &Matrix{cols: cols, rows: rows, dim: rows * cols, data: make([]float64, rows*cols)}
+	m := Matrix{cols: cols, rows: rows, dim: rows * cols, data: make([]float64, rows*cols)}
 	copy(m.data, data)
-	return m, nil
+	return m
 }
 
 //Returns an empty matrix
-func NewZeroMatrix(cols, rows int) *Matrix {
-	m := &Matrix{cols: cols, rows: rows, dim: rows * cols, data: make([]float64, rows*cols)}
+func NewZeroMatrix(cols, rows int) Matrix {
+	m := Matrix{cols: cols, rows: rows, dim: rows * cols, data: make([]float64, rows*cols)}
 	for i := range m.data {
 		m.data[i] = 0
 	}
 	return m
 }
 
-func (m *Matrix) Scalar(a float64) {
+//Dot Product
+func Product(a, b Matrix) Matrix {
+	if a.cols != b.rows {
+		log.Fatalf("The number of A cols (%v) doesn't match the number of B cols (%v)", a.cols, b.cols)
+	}
+	c := Matrix{rows: a.rows, cols: b.cols, dim: a.rows * b.cols, data: make([]float64, a.rows*b.cols)}
+	for i := 0; i < a.rows; i++ {
+		for j := 0; j < b.cols; j++ {
+			for k := 0; k < c.cols; k++ {
+				c.data[i*c.cols+k] += a.data[i*a.cols+j] * b.data[j*b.cols+k]
+			}
+		}
+	}
+	return c
+}
+
+func (m Matrix) Scalar(a float64) {
 	for i := 0; i < m.rows; i++ {
 		for j := 0; j < m.cols; j++ {
 			m.data[i*m.cols+j] *= a
@@ -39,7 +55,7 @@ func (m *Matrix) Scalar(a float64) {
 	}
 }
 
-func (m *Matrix) Show() {
+func (m Matrix) Show() {
 	for i := 0; i < m.rows; i++ {
 		for j := 0; j < m.cols; j++ {
 			fmt.Print(m.data[i*m.cols+j], " ")
